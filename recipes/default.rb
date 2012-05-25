691edb7b52c1d3c009ab2b9343eb0ed5abf99c57
Lars Bilke <lars.bilke@ufz.de>
2012-05-25 13:54:57 +0200

    ssh config

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,15 @@ end
 package "libvtk5-qt4-dev" do
 	action :install
 end
+
+cookbook_file "/home/vagrant/.ssh/config" do
+	source "ssh_config"
+	mode "0644"
+	owner "vagrant"
+end
+
+cookbook_file "/home/vagrant/.ssh/known_hosts" do
+	source "ssh_known_hosts"
+	mode "0600"
+	owner "vagrant"
+end
